234d16fae3c8a1408ab1393d3c86b55620738ff5
Dom Sinclair <dom@viewtolearn.co.uk>
2026-01-09 19:34:04 +0000

    fix: home module routes updated.

--- Modules/Home/routes.php
+++ Modules/Home/routes.php
@@ -1,8 +1,9 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
-use Ishmael\Core\Router;
-use Modules\Home\Controllers\HomeController;
+    use Ishmael\Core\Router;
+
+    use Modules\Home\Controllers\HomeController;
 
     return function (Router $router): void {
         $router->get('/', [HomeController::class, 'index']);
